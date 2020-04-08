1d001cbea25bb1b324a8026271d83872073d7424
Tiger <thisisstreamliner@gmail.com>
2020-04-08 18:01:05 +0300

    Navigation and setup UI

---
+++ app/src/main/java/com/thisisstreamliner/forecastmvvm/ui/weather/current/CurrentWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package com.thisisstreamliner.forecastmvvm.ui.weather.current
+
+import androidx.lifecycle.ViewModel
+
+class CurrentWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
